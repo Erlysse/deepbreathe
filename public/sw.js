ce4225dd3eace98191e302abb9534591a15851f6
Florian L <f.lvsr@protonmail.com>
2026-01-09 18:29:52 +0100

    Fix 404 and PWA installation

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,11 @@
 
-const CACHE_NAME = 'deep-breathe-v18';
+const CACHE_NAME = 'deep-breathe-v19';
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
-  '/manifest.json?v=18',
+  '/manifest.json?v=19',
   'https://cdn.tailwindcss.com',
-  'https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500&family=Playfair+Display:ital@0;1&display=swap',
-  'https://cdn-icons-png.flaticon.com/512/2665/2665178.png?v=18'
+  'https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500&family=Playfair+Display:ital@0;1&display=swap'
 ];
 
 self.addEventListener('install', (event) => {
